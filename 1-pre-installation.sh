0b48bd688831f2227554d7b4bab9db92e35c5dcc
Artemee <artemee.senin@gmail.com>
2018-09-26 19:45:16 +0300

    Update 1-pre-installation.sh

--- 1-pre-installation.sh
+++ 1-pre-installation.sh
@@ -8,8 +8,11 @@ cfdisk /dev/nvme0n1
 mkfs.fat -F32 /dev/nvme0n1p1
 mkfs.ext4 -L Root /dev/nvme0n1p2
 mkfs.ext4 -L Home /dev/nvme0n1p3
+lsblk --fs
 mount /dev/nvme0n1p3 /mnt
 mkdir /mnt/{boot,home}
 mount /dev/nvme0n1p1 /mnt/boot
 mount /dev/nvme0n1p3 /mnt/home/
+mkdir /mnt/mnt/Storage
+mount /dev/sda1 /mnt/mnt/Storage
 lsblk -o name,mountpoint,label,size,uuid
