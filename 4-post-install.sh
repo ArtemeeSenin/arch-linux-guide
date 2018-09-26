f608afec638afee1d1f18f69c3a13b685e32fb9f
Artemee <artemee.senin@gmail.com>
2018-09-27 02:53:40 +0300

    Update 4-post-install.sh

--- 4-post-install.sh
+++ 4-post-install.sh
@@ -7,7 +7,7 @@ echo "exec startkde" > ~/.xinitrc
 sudo systemctl enable sddm.service
 sudo systemct disable dhcpcd
 
-sudo pacman -S xf86-input-synaptics
+sudo pacman -S xf86-input-synaptics xf86-video-intel mesa
 
 reboot
 
@@ -16,8 +16,13 @@ sudo systemctl enable NetworkManager
 sudo ip link set wlo1 up
 sudo systemctl enable NetworkManager
 
-sudo pacman -S firefox git xf86-video-intel mesa
+sudo pacman -S firefox git
 
 git clone https://aur.archlinux.org/pikaur.git
 cd pikaur
 makepkg -fsri
+
+#install jetbrains-toolbox
+#install nvm
+#install langs
+#install bluetooth
