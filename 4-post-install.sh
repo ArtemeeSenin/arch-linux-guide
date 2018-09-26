112af936751093fadced1c9b94a38b53a9e866a1
Artemee <artemee.senin@gmail.com>
2018-09-27 02:44:30 +0300

    Update 4-post-install.sh

--- 4-post-install.sh
+++ 4-post-install.sh
@@ -2,3 +2,14 @@ sudo pacman -S xorg xorg-init xorg-server xorg-apps xterm
 sudo pacman -S plasma kde-applications sddm
 
 Xorg :0 -configure
+
+sudo systemctl enable sddm.service
+
+sudo ip link set wlo1 up
+sudo systemctl enable NetworkManager
+
+sudo pacman -S firefox git xf86-video-intel mesa
+
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur
+makepkg -fsri
