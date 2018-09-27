3a184d8aa19f8acc4a5a3d4df80746058999469f
Artemee <artemee.senin@gmail.com>
2018-09-27 12:10:35 +0300

    Update 4-post-install.sh

--- 4-post-install.sh
+++ 4-post-install.sh
@@ -1,15 +1,18 @@
-sudo pacman -S xorg xorg-init xorg-server xorg-apps xorg-utils xterm
-sudo pacman -S plasma kde-applications sddm
+pacman -S xorg xorg-init xorg-server xorg-apps xorg-utils xterm
+pacman -S plasma kde-applications sddm
 
 Xorg :0 -configure
+cp /root/xorg.conf.new /etc/X11/xorg.conf
 
 echo "exec startkde" > ~/.xinitrc
-sudo systemctl enable sddm.service
-sudo systemct disable dhcpcd
+systemctl enable sddm.service
+systemct disable dhcpcd
 
-sudo pacman -S xf86-input-synaptics xf86-video-intel mesa
+pacman -S xf86-input-synaptics xf86-video-intel mesa alsa-utils alsa-plugins
 
 reboot
+
+# Log in and install software
 
 sudo systemctl enable NetworkManager 
 
@@ -18,14 +21,35 @@ sudo systemctl enable NetworkManager
 
 sudo pacman -S firefox git
 
+# pikaur
 git clone https://aur.archlinux.org/pikaur.git
 cd pikaur
 makepkg -fsri
 
 #install jetbrains-toolbox
+pikaur -S jetbrains-toolbox --noconfirm
+
+# telegram
+pikaur -S telegram-desktop --noconfirm
+
+# zsh
+sudo pacman -S zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /bin/zsh
+
+# nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-echo "node" > .nvmrc # to default to the latest version
+nvm install --latest-npm
+echo "node" > .nvmrc
 
 echo -e 'export NVM_DIR="$HOME/.nvm"\n[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm\n[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' > ~/.zshrc
-#install langs'
+# lang switching
+setxkbmap -layout us,ru -option 'grp:caps_toggle,grp_led:caps'
+setxkbmap -layout us,ru -option 'grp:alt_shift_toggle'
+
 #install bluetooth
+sudo pacman -S bluez bluez-utils blueman pulseaudio pulseaudio-bluetooth
+
+# fonts
+pikaur -S powerline-fonts
+curl -L https://github.com/caarlos0/msfonts/raw/master/install.sh | sudo sh
